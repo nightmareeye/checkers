2fb394e5f495b31e8a59a7568f22a1f1947984db
xSkywa1ker <you@example.com>
2021-12-13 05:46:50 +0300

    укцкуу

--- Сhecker/Checkers.cpp
+++ Сhecker/Checkers.cpp
@@ -4,19 +4,19 @@ using namespace sf;
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 int main() {
-	setlocale(LC_ALL, "rus");
-	Game game;
+	setlocale(LC_ALL, "rus"); // установка русского языка
+	Game game; // игра
 	bool start = 1;
-	RenderWindow window(VideoMode(800, 800), "Checker");
+	RenderWindow window(VideoMode(800, 800), "Checker"); // окно программы
 	while (window.isOpen()) {
 		Event event; 
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed)
 				window.close();
-			game.make_move(window, event);
+			game.make_move(window, event); 
 		}
 		window.clear(Color(236, 200, 159));
-		game.get_checkers_on_board().draw_checkers(window);//рисую поле и шашки
+		game.get_checkers_on_board().draw_checkers(window); // рисую поле и шашки
 		window.display();
 	}
 	return 0;
